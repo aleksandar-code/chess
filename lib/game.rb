089da677fd9e04839b2fbdc4c91a5a85b01c5513
aleksandar-code <aca.jevdjevic@gmail.com>
2022-10-29 15:24:29 +0200

    Creates classes & script file

---
+++ lib/game.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class Game
+    def initialize
+        
+    end
+end
---
+++ lib/script.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative './game'
+require_relative './board'
+require_relative './player'
+
+chess = Game.new
+chess.play
