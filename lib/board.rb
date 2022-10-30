929c68105c0743bd51660b3f2a40c4548466b771
aleksandar-code <aca.jevdjevic@gmail.com>
2022-10-30 10:09:13 +0100

    Create a board and print_board

--- lib/board.rb
+++ lib/board.rb
@@ -4,12 +4,48 @@ require 'pry-byebug'
 
 class Board
   def initialize
-    @green = "\e[1;42m   \e[0m"
-    @white = "\e[1;47m   \e[0m"
+    @green = "\e[1;42m    \e[0m"
+    @white = "\e[1;47m    \e[0m"
     @board = create_board
   end
 
   def print_board
-    @board.each { |x| x.each { |n|  print n} }
+    @board.each do |x|
+    2.times do
+      x.each do |n|
+        print n
+      end
+      puts "\n"
+    end
+    end
   end
+
+  def create_board
+    id = false
+    final_array = []
+    j = 0
+    8.times do
+      
+      if j.odd?
+        id = true
+      else
+        id = false
+      end
+      j += 1
+      arr = Array.new(8)
+      final_array << arr.each_with_index do |x, idx|
+        if id == false && arr[idx].nil?
+          arr[idx] = @white 
+          id = true
+        end
+  
+        if id == true && arr[idx].nil?
+          arr[idx] = @green
+          id = false
+        end
+      end
+    end
+    final_array
+  end
+
 end
