9d51f7b540842cafe52ac64128cac982a8271045
aleksandar-code <aca.jevdjevic@gmail.com>
2022-10-30 10:50:05 +0100

    Print board

--- lib/board.rb
+++ lib/board.rb
@@ -4,19 +4,17 @@ require 'pry-byebug'
 
 class Board
   def initialize
-    @green = "\e[1;42m    \e[0m"
-    @white = "\e[1;47m    \e[0m"
+    @green = "\e[1;42m   \e[0m"
+    @white = "\e[1;47m   \e[0m"
     @board = create_board
   end
 
   def print_board
     @board.each do |x|
-    2.times do
       x.each do |n|
         print n
       end
       puts "\n"
-    end
     end
   end
 
@@ -25,7 +23,6 @@ class Board
     final_array = []
     j = 0
     8.times do
-      
       if j.odd?
         id = true
       else
@@ -37,9 +34,7 @@ class Board
         if id == false && arr[idx].nil?
           arr[idx] = @white 
           id = true
-        end
-  
-        if id == true && arr[idx].nil?
+        elsif id == true && arr[idx].nil?
           arr[idx] = @green
           id = false
         end
