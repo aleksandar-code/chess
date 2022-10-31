3117bfbf794d7fd0bf6294c622d3fa96734c7d8a
aleksandar-code <aca.jevdjevic@gmail.com>
2022-10-31 10:17:13 +0100

    Update comments

--- lib/board.rb
+++ lib/board.rb
@@ -14,7 +14,8 @@ require_relative './graph'
 class Board
   def initialize
     @green = "\e[1;40m   \e[0m" # put them to be nodes directly and then print the color and piece etc
-    @white = "\e[1;47m   \e[0m"
+    @white = "\e[1;47m   \e[0m" # now that i have coords in my nodes i want to have the real board coords like a8 a7 etc
+    # it can be easily obtainable.
     @graph = Graph.new
     @board = create_board
   end
