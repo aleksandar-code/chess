a4a1d0e6e1b255d7722a3c06c4fc43fdae84229e
aleksandar-code <aca.jevdjevic@gmail.com>
2022-11-01 09:40:05 +0100

    Update king

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -2,6 +2,11 @@
 
 class King
   def initialize
-    
+    @unicode = '♚'
+    @start_white = ['e1']
+    @start_black = ['e8']
+    @check_status = nil
+    @current_position = nil # Will use graph of nodes to see where this one can move
   end
+  attr_accessor :current_position, :check_status
 end
