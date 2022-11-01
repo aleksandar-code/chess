46ad72f187d9204bf10ac8a3c321dd3654247a3f
aleksandar-code <aca.jevdjevic@gmail.com>
2022-11-01 09:38:17 +0100

    Update pawn

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -4,7 +4,7 @@ class Pawn
   def initialize
     @unicode = '♟︎'
     @start_white = ['a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2']
-    @start_black = ["a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"]
+    @start_black = ["a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"]
     @en_passant = nil
     @current_position = nil # Will use graph of nodes to see where this one can move
   end
