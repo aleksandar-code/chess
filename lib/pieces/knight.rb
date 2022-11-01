2bbeef1b25a56fd5281e4a6f57978e5942f893a1
aleksandar-code <aca.jevdjevic@gmail.com>
2022-11-01 09:38:03 +0100

    Update knight

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -2,6 +2,10 @@
 
 class Knight
   def initialize
-    
+    @unicode = '♞'
+    @start_white = ['b1', 'g1']
+    @start_black = ["b8", "g8"]
+    @current_position = nil # Will use graph of nodes to see where this one can move
   end
+  attr_accessor :current_position
 end
