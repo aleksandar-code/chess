1ed43b1dbcb2ca04ebde92b6a3dc8f101a9ba13b
aleksandar-code <aca.jevdjevic@gmail.com>
2022-11-01 09:41:19 +0100

    Update bishop

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -2,6 +2,10 @@
 
 class Bishop
   def initialize
-    
+    @unicode = '♝'
+    @start_white = ['c1', 'f1']
+    @start_black = ["c8", "f8"]
+    @current_position = nil # Will use graph of nodes to see where this one can move
   end
+  attr_accessor :current_position
 end
