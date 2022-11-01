2fc81adf828ac7e87affedcdd8525d83f03e1d5f
aleksandar-code <aca.jevdjevic@gmail.com>
2022-11-01 09:49:06 +0100

    Update rook

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 class Rook
-  def initialize
+  def initialize(color, current_position = nil)
     @unicode = '♜'
     @start_white = ['a1', 'h1']
     @start_black = ['a8', 'h8']
-    @current_position = nil # Will use graph of nodes to see where this one can move
+    @color = color # Will use graph of nodes to see where this one can move
+    @current_position = current_position
   end
-  attr_accessor :current_position
+  attr_accessor :current_position, :color
 end
