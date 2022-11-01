723f251933b6bb9188194b5db31d7be7b25dfc90
aleksandar-code <aca.jevdjevic@gmail.com>
2022-11-01 09:32:27 +0100

    Update queen

--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -2,6 +2,10 @@
 
 class Queen
   def initialize
-    
+    @unicode = '♛'
+    @start_white = ['d1']
+    @start_black = ['d8']
+    @current_position = nil # Will use graph of nodes to see where this one can move
   end
+  attr_accessor :current_position
 end
