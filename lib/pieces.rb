6330a724923273ab8ed5873da47444d3e7258259
aleksandar-code <aca.jevdjevic@gmail.com>
2022-11-01 10:08:43 +0100

    Create pieces.Rb

---
+++ lib/pieces.rb
@@ -0,0 +1,6 @@
+require_relative './pieces/king'
+require_relative './pieces/queen'
+require_relative './pieces/rook'
+require_relative './pieces/knight'
+require_relative './pieces/bishop'
+require_relative './pieces/pawn'
