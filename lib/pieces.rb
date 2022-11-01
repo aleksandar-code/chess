98c7e5237e1331ff42f6f4b6fec9f9057e030e56
aleksandar-code <aca.jevdjevic@gmail.com>
2022-11-01 10:09:44 +0100

    Create pieces class

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -1,6 +1,14 @@
+# frozen_string_literal: true
+
 require_relative './pieces/king'
 require_relative './pieces/queen'
 require_relative './pieces/rook'
 require_relative './pieces/knight'
 require_relative './pieces/bishop'
 require_relative './pieces/pawn'
+
+class Pieces
+    def initialize
+        
+    end
+end
