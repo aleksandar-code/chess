9625d0e0d03754604efc17824a80fc00af3269e4
aleksandar-code <aca.jevdjevic@gmail.com>
2022-11-02 16:00:37 +0100

    Update rook

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -7,5 +7,5 @@ class Rook
     @start_black = %w[a8 h8]
     # Will use graph of nodes to see where this one can move
   end
-  attr_accessor :piece
+  attr_accessor :piece, :start_white, :start_black
 end
