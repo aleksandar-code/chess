2dc162cb7ddf9e6978aa207721f8f7522fe1b2bb
aleksandar-code <aca.jevdjevic@gmail.com>
2022-11-02 17:50:57 +0100

    Update pieces, add create_instances

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -22,24 +22,70 @@ class Pieces
     @black_pieces << piece unless color.zero?
   end
 
-  def create_black_pieces
+  def create_instances(array_pieces, state)
+    array = []
+    if state > 0
+      arr = create_pieces(array_pieces)
+      arr.each do |x|
+        array << x
+      end
+      array_pieces[8..].each do |piece|
+        array << Pawn.new(piece)
+      end
+      p array
+    else
+      array_pieces[8..].each do |piece|
+        array << Pawn.new(piece)
+      end
+      arr = create_pieces(array_pieces)
+      arr.each do |x|
+        array << x
+      end
+      p array
+    end
+  end
+
+  def create_pieces(array_pieces)
+    array = []
+    array << Rook.new(array_pieces[0])
+    array << Knight.new(array_pieces[1])
+    array << Bishop.new(array_pieces[2])
+    array << Queen.new(array_pieces[3])
+    array << King.new(array_pieces[4])
+    array << Bishop.new(array_pieces[5])
+    array << Knight.new(array_pieces[6])
+    array << Rook.new(array_pieces[7])
+    array
+  end
+
+  def create_white_pieces
     black = "\e[1;31m "
     i = 0
+    array = []
     @pieces.length.times do
       piece = @pieces[i]
-      to_send = Rook.new(set_color(black.dup, piece))
-      add_piece(1, to_send)
+      i += 1
+      array << (set_color(black.dup, piece))
+    end
+    array = create_instances(array, 1)
+    array.each do |piece|
+      add_piece(1, piece)
     end
   end
 
   # how to create all of them? 
-  def create_white_pieces
+  def create_black_pieces
     white = "\e[1;34m "
     i = 0
+    array = []
     @pieces.length.times do
       piece = @pieces[i]
-      to_send = Rook.new(set_color(white.dup, piece))
-      add_piece(0, to_send)
+      i += 1
+      array << (set_color(white.dup, piece))
+    end
+    array = create_instances(array, 0)
+    array.each do |piece|
+      add_piece(0, piece)
     end
   end
 
