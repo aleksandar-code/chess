4eb07c2b570e0b766517faa844c73cf4ca54eb73
aleksandar-code <aca.jevdjevic@gmail.com>
2022-11-02 18:13:17 +0100

    Update knight

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 class Knight
-  def initialize(color)
-    @unicode = '♞'
+  def initialize(piece)
+    @piece = piece
     @start_white = %w[b1 g1]
     @start_black = %w[b8 g8]
-    @color = color
   end
   attr_accessor :piece, :start_white, :start_black
 end
