c14a6b0aad83278cb8df08287ee773a0d097b442
aleksandar-code <aca.jevdjevic@gmail.com>
2022-11-02 18:13:49 +0100

    Update bishop

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 class Bishop
-  def initialize(color)
-    @unicode = '♝'
+  def initialize(piece)
+    @piece = piece
     @start_white = %w[c1 f1]
     @start_black = %w[c8 f8]
-    @color = color
   end
   attr_accessor :piece, :start_white, :start_black
 end
