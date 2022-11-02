8ddefb02087a744d9645d3330b04e19263c11f05
aleksandar-code <aca.jevdjevic@gmail.com>
2022-11-02 18:13:34 +0100

    Update king

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
 class King
-  def initialize(color)
-    @unicode = '♚'
+  def initialize(piece)
+    @piece = piece
     @start_white = ['e1']
     @start_black = ['e8']
     @check_status = nil
-    @color = color
   end
   attr_accessor :piece, :start_white, :start_black
 end
