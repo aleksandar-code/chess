4eb73df9e447b43f9210a6409ba81edb66c4bf45
aleksandar-code <aca.jevdjevic@gmail.com>
2022-11-04 10:30:10 +0100

    Update node class

--- lib/node.rb
+++ lib/node.rb
@@ -28,12 +28,8 @@ class Node
     arr_i = %w[8 7 6 5 4 3 2 1]
     arr_j = %w[a b c d e f g h]
 
-    i = data[0]
-
-    j = data[1]
-
-    data[1] = arr_i[i]
-    data[0] = arr_j[j]
+    data[1] = arr_i[data[0]]
+    data[0] = arr_j[data[1]]
 
     data[0] + data[1]
   end
