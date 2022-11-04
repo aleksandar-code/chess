722b13722fd6c984c8ebeaed5cd3f5ef1a921525
aleksandar-code <aca.jevdjevic@gmail.com>
2022-11-04 10:08:49 +0100

    Add player_color test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,1 +1,27 @@
 # frozen_string_literal: true
+
+require_relative '../lib/game'
+
+RSpec.describe Game do
+
+  describe '#player_color' do
+  subject(:game_color) { Game.new }
+    context 'when turn is zero' do
+  
+      it 'returns white' do
+        expect(game_color.player_color).to eq('white')
+      end
+    end
+  end
+
+  describe '#switch_player' do
+    
+    context 'when turn is zero' do
+
+      it 'returns 1' do
+        
+      end
+    end
+  end
+
+end
