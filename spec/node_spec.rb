bdcd93e9c509dae150d65f84034a88f730683963
aleksandar-code <aca.jevdjevic@gmail.com>
2022-11-04 10:45:23 +0100

    Add test for node

---
+++ spec/node_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+RSpec.describe Node do
+  
+  describe '#convert_data' do
+
+    context 'when data with [0, 0] is given' do
+
+      it 'returns a8' do
+
+      end
+    end
+  end
+  
+end
