719cb2dc819d4bbdb21452313435187fade9734f
aleksandar-code <aca.jevdjevic@gmail.com>
2022-11-04 11:14:21 +0100

    Add piece_print test

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Node do
   describe '#convert_data' do
   
     context 'when data with [0, 0] is given' do
-    subject(:node_data) { Node.new([0, 0], "e") }
+    subject(:node_data) { Node.new([0, 0], "\e[1;47m   \e[0m") }
       
       it 'returns a8' do
         expect(node_data.coords).to eq('a8')
@@ -16,16 +16,14 @@ RSpec.describe Node do
   end
 
   describe '#piece_print' do
-  let(:node_data) { instance_double(Node) }
+  subject(:node_print) { Node.new([0, 0], "\e[1;47m   \e[0m", "\e[1;34m♜") }
 
-    context 'when data with [0, 0] is given' do
-      
-      before do
-        allow(node_data).to receive(:convert_data).with([0, 0]).and_return('a8')
-      end
+    context 'when a square and a piece are given' do
 
-      it 'returns a8' do
-        expect(node_data.convert_data([0, 0])).to eq('a8')
+      it 'returns the square with the piece in one string' do
+        sqr = "\e[1;47m   \e[0m"
+        result = "\e[1;47m \e[1;34m♜ \e[0m"
+        expect(node_print.piece_print(sqr.dup)).to eq(result)
       end
     end
   end
