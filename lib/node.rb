95d6658e6626214a3f4042d4ddd13fbc94eadbd9
aleksandar-code <aca.jevdjevic@gmail.com>
2022-11-04 10:43:39 +0100

    Refactor convert_data

--- lib/node.rb
+++ lib/node.rb
@@ -28,9 +28,6 @@ class Node
     arr_i = %w[8 7 6 5 4 3 2 1]
     arr_j = %w[a b c d e f g h]
 
-    data[1] = arr_i[data[0]]
-    data[0] = arr_j[data[1]]
-
-    data[0] + data[1]
+    arr_j[data[1]] + arr_i[data[0]]
   end
 end
