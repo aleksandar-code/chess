492e9b29c279ec9354949d97c72e79c5f090479b
aleksandar-code <aca.jevdjevic@gmail.com>
2022-11-06 16:31:53 +0100

    Update King

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 
 class King
-  def initialize(piece)
+  def initialize(piece, id)
     @piece = piece
     @start_white = ['e1']
     @start_black = ['e8']
     @check_status = nil
     @current_position = nil
     @board = nil
+    @id = id
+    @moves = moves
     @move_pattern = [[-1, -1, -1, 0, 0, +1, +1, +1], [0, +1, -1, +1, -1, -1, +1, 0]]
   end
-  attr_accessor :piece, :start_white, :start_black, :current_position, :board
+  attr_accessor :piece, :start_white, :start_black, :current_position, :board, :moves
 end
