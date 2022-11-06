781e2a05c7866a5c09da0622ba5e046638faba0c
aleksandar-code <aca.jevdjevic@gmail.com>
2022-11-06 16:31:14 +0100

    Update knight

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
 class Knight
-  def initialize(piece)
+  def initialize(piece, id)
     @piece = piece
     @start_white = %w[b1 g1]
     @start_black = %w[b8 g8]
     @current_position = nil
     @board = nil
+    @id = id
+    @moves = nil
     @move_pattern =[[-2, -2, -1, -1, +2, +2, +1, +1], [-1, +1, -2, +2, -1, +1, -2, +2]]
   end
-  attr_accessor :piece, :start_white, :start_black, :current_position, :board
+  attr_accessor :piece, :start_white, :start_black, :current_position, :board, :id, :moves
 end
