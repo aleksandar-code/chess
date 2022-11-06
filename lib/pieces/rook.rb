4e5225b47736ff97e4b863981406665ad9c148f3
aleksandar-code <aca.jevdjevic@gmail.com>
2022-11-06 15:57:03 +0100

    Update rook

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -3,17 +3,19 @@
 require 'pry-byebug'
 
 class Rook
-  def initialize(piece)
+  def initialize(piece, id)
     @piece = piece
     @start_white = %w[a1 h1]
     @start_black = %w[a8 h8]
     @current_position = nil
     @board = nil
+    @id = id
+    @moves = nil
     @move_pattern = [[+1, 0, -1, 0], [0, +1, 0, -1]]
     # Will use graph of nodes to see where this one can move
     # this one can move in 4 directions calculate after every move
   end
-  attr_accessor :piece, :start_white, :start_black, :current_position, :board
+  attr_accessor :piece, :start_white, :start_black, :current_position, :board, :moves, :id
 
   # write rules for the rook.
 
