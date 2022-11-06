106444d8de754084876535572b8134f84df05271
aleksandar-code <aca.jevdjevic@gmail.com>
2022-11-06 16:11:58 +0100

    Update queen

--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
 class Queen
-  def initialize(piece)
+  def initialize(piece, id)
     @piece = piece
     @start_white = ['d1']
     @start_black = ['d8']
     @current_position = nil
     @board = nil
+    @id = id
+    @moves = nil
     @move_pattern = [[+1, 0, -1, 0, -1, +1, -1, +1], [0, +1, 0, -1, -1, -1, +1, +1]]
   end
-  attr_accessor :piece, :start_white, :start_black, :current_position, :board
+  attr_accessor :piece, :start_white, :start_black, :current_position, :board, :moves, :id
 end
