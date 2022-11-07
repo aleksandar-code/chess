7e4054e0d326f4f119f80f52a9141cbd88ae1501
aleksandar-code <aca.jevdjevic@gmail.com>
2022-11-07 08:26:54 +0100

    Add promo variables

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -13,6 +13,8 @@ class Pawn
     @piece = piece
     @start_white = %w[a2 b2 c2 d2 e2 f2 g2 h2]
     @start_black = %w[a7 b7 c7 d7 e7 f7 g7 h7]
+    @promo_white = ["a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"]
+    @promo_black = ["a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"]
     @current_position = nil
     @id = id
     @board = nil
