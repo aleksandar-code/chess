0ecb868388399dca6f45215303606cf334a21cfe
aleksandar-code <aca.jevdjevic@gmail.com>
2022-11-08 08:30:05 +0100

    Rename function verify_node

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -11,15 +11,10 @@ class Rook
     @board = nil
     @id = id
     @moves = nil
-    @move_pattern = [[+1, 0, -1, 0], [0, +1, 0, -1]]
     # Will use graph of nodes to see where this one can move
     # this one can move in 4 directions calculate after every move
   end
   attr_accessor :piece, :start_white, :start_black, :current_position, :board, :moves, :id
-
-  def get_pattern
-    @move_pattern
-  end
 
   def can_move
     # check if a piece is in front "ally or enemy for pawn" if ally
@@ -34,31 +29,30 @@ class Rook
   end
 
   def calc_move(start, destination, p_id)
-    pattern = get_pattern
     return false if @id != p_id
     strt = find_piece(start)
     dest = find_piece(destination)
 
-    possible_moves(strt, dest, pattern)
+    possible_moves(strt, dest)
 
     return true if moves.include?(dest)
     false
   end
 
-  def possible_moves(strt, dest, pattern)
+  def possible_moves(strt, dest)
     # up
     current_coords = strt
     node_start = find_node(current_coords)
     node_destination = find_node(dest)
     stop = false
     until stop
-      
+
     end
 
     # create way for rook to go over the square
   end
 
-  def is_valid?(node)
+  def verift_node(node)
 
   end
 
