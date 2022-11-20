db06c1573c7c4badaedc8fed574b6d5cd2aa73bf
aleksandar-code <aca.jevdjevic@gmail.com>
2022-11-20 19:05:57 +0100

    Update

--- lib/script.rb
+++ lib/script.rb
@@ -6,14 +6,12 @@ require_relative './game'
 require_relative './board'
 require_relative './player'
 
-
 print "Want to load a saved game? Enter 'replay': "
 do_load = gets.chomp
 
 $chess = Game.new unless do_load == "replay"
 
 if Dir.exist?('saved_games') && do_load == "replay"
-    
     game_file = File.new("saved_games/saved.yaml")
     yaml = game_file.read
     $chess = Psych.unsafe_load(yaml)
