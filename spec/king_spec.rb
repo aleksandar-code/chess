0fa552cdee9f630b519b604fefe85f8a92db7728
aleksandar-code <aca.jevdjevic@gmail.com>
2022-11-22 18:30:34 +0100

    Add tests

--- spec/king_spec.rb
+++ spec/king_spec.rb
@@ -3,6 +3,28 @@
 require_relative '../lib/pieces/king'
 
 RSpec.describe King do
-  
+  describe '#stalemate' do
+    context 'when' do
+      it 'returns something' do
+
+      end
+    end
+  end
+
+  describe '#check_mate' do
+    context 'when' do
+      it 'returns something' do
+        
+      end
+    end
+  end
+
+  describe '#test_moves' do
+    context 'when' do
+      it 'returns something' do
+        
+      end
+    end
+  end
   
 end
