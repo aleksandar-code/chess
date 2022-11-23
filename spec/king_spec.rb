6b06506c0281dfdfa5b7eca050dfbfc292ac7a73
aleksandar-code <aca.jevdjevic@gmail.com>
2022-11-23 12:38:13 +0100

    Update

--- spec/king_spec.rb
+++ spec/king_spec.rb
@@ -38,6 +38,41 @@ RSpec.describe King do
   end
 
   describe '#check_mate' do
+    context 'when a position is not a check_mate' do
+      let(:game) { Game.new }
+      let(:king) { King.new(0, 0) }
+      let(:board) { Board.new }
+
+      before do
+        allow(king).to receive(:get_all_moves).and_return([6, 0], [5, 0])
+        allow(king).to receive(:test_moves).and_return([6, 0], [5, 0])
+      end
+
+      it 'returns false' do
+        expect(king.check_mate(0)).to be(false)
+      end
+    end
+
+    context 'when a position is a check_mate' do
+      let(:game) { Game.new }
+      let(:king) { King.new(0, 0) }
+      let(:board) { Board.new }
+      before do
+        allow(king).to receive(:get_all_moves).and_return([6, 0], [5, 0])
+        allow(king).to receive(:test_moves).and_return(["check", "check"])
+      end
+
+      it 'returns true' do
+        expect(king.check_mate(0)).to be(true)
+      end
+    end
+  end
+
+  describe '#test_moves' do
+    let(:game) { Game.new }
+    let(:king) { King.new(0, 0) }
+    let(:board) { Board.new }
+    
     context 'when' do
       it 'returns something' do
         
@@ -45,7 +80,7 @@ RSpec.describe King do
     end
   end
 
-  describe '#test_moves' do
+  describe '#castling' do
     context 'when' do
       it 'returns something' do
         
